8a88ef4181d3b18bb1936951019a8508b2529d32
Jeffrey Degoma <jhdegoma@hotmail.com>
2021-01-07 20:31:44 -0500

    loader

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-$( document ).ready(() => {
+const handle = () => {
+    alert('hi')
+}
+// setTimeout(handle,2000)
+$( window ).on('load',() => {
+    $('#cover').fadeOut(2000);
 
     (function(){
+
         $('.contact-container').hide()
         $('.about-container').hide()
         $('.project-container').hide()
@@ -10,16 +16,20 @@ $( document ).ready(() => {
     const year = date.getFullYear();
     const $mainContent = $('.main-content')
 
-    const showProjects = () => {
-        $mainContent.children().hide()
-        $('.project-container').fadeIn()
-    }
+
 
     const showIntro = () => {
         $mainContent.children().hide()
         $('#hero').parent().siblings().removeAttr('style')
         $('.hero-container').fadeIn()
     }
+
+    const showProjects = () => {
+        $mainContent.children().hide()
+        $('.project-container').fadeIn()
+    }
+
+   
 
     const showAbout = () => {
         $mainContent.children().hide()
